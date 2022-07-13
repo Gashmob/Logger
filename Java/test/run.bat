6ead4eab909bbccd0b71bac5d5a407280b606848
Kevin Traini <kevin@ktraini.com>
2022-07-13 22:00:39 +0200

    Comment test-windows waiting to find a solution

--- Java/test/run.bat
+++ Java/test/run.bat
@@ -1,6 +1,5 @@
 :: Compile tests
-javac -cp .\logger -sourcepath .\test\*.java
-::javac -cp .\logger .\test\*.java
+javac -cp .\logger .\test\*.java
 
 :: Run tests
-::java -cp .\logger test.Main
+java -cp .\logger test.Main
