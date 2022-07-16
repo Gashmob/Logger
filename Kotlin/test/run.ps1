f92e0317930572472486848a7c54786c7ac03920
Kevin Traini <kevin@ktraini.com>
2022-07-16 17:33:03 +0200

    Kotlin : Comment windows test in waiting to find a solution

--- Kotlin/test/run.ps1
+++ Kotlin/test/run.ps1
@@ -1,5 +1,5 @@
 # Compile tests
-kotlinc .\test\*.kt
+kotlinc -cp Logger_Kotlin.jar ./test/*.kt -include-runtime
 
 # Run tests
-#kotlin -cp Logger_Kotlin.jar; MainKt
+kotlin -cp Logger_Kotlin.jar; MainKt
