9febf5bcaf782784c6e1b2362e6a1c15709dd9a0
Kevin Traini <kevin@ktraini.com>
2022-07-16 17:17:53 +0200

    Kotlin : Try fix run.bat

--- Kotlin/test/run.bat
+++ Kotlin/test/run.bat
@@ -1,6 +1,6 @@
 :: Compile tests
-kotlinc -cp Logger_Kotlin.jar; .\test\*.kt -include-runtime
+dir
+::kotlinc -cp Logger_Kotlin.jar; .\test\*.kt -include-runtime
 
 :: Run tests
-kotlin -cp Logger_Kotlin.jar; MainKt
-java -cp Logger_Java.jar; test.Main
+::kotlin -cp Logger_Kotlin.jar; MainKt
