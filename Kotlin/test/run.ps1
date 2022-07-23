407e0323371be56cf8cbcd749c0815c68f94f271
Kevin Traini <kevin@ktraini.com>
2022-07-23 15:15:13 +0200

    Kotlin : rename run.ps1 -> run.bat

--- Kotlin/test/run.ps1
+++
@@ -1,6 +0,0 @@
-# Compile tests
-# kotlinc -cp Logger_Kotlin.jar ./test/*.kt -include-runtime
-kotlinc -cp Logger_Kotlin.jar ./test/main.kt -include-runtime
-
-# Run tests
-kotlin -cp Logger_Kotlin.jar; MainKt
