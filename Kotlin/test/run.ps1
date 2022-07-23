613a5448fdf5132fe12a09b62a0dc320968c183a
Kevin Traini <kevin@ktraini.com>
2022-07-23 10:33:13 +0200

    Kotlin : try fix run.ps1

--- Kotlin/test/run.ps1
+++ Kotlin/test/run.ps1
@@ -1,5 +1,6 @@
 # Compile tests
-kotlinc -cp Logger_Kotlin.jar ./test/*.kt -include-runtime
+# kotlinc -cp Logger_Kotlin.jar ./test/*.kt -include-runtime
+kotlinc -cp Logger_Kotlin.jar ./test/main.kt -include-runtime
 
 # Run tests
 kotlin -cp Logger_Kotlin.jar; MainKt
